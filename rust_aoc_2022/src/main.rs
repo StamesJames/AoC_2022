de1367357666af4f4d4ad76bf67329c325392732
Stames Laptop 001 <benedict.smit@udo.edu>
2022-12-01 16:29:54 +0100

    made the prints a bit better

--- rust_aoc_2022/src/main.rs
+++ rust_aoc_2022/src/main.rs
@@ -4,6 +4,14 @@ use rust_aoc_2022::aoc_lib::elf_food::{find_most_cal_elf, find_3_most_cal_elfs};
 
 
 fn main() {
-    //println!("{:?}", find_most_cal_elf(&Path::new(r"./res/day_1/elf_food_list.csv")));
-    println!("{:?}", find_3_most_cal_elfs(&Path::new(r"./res/day_1/elf_food_list.csv")).iter().fold(0, |x,y| x+y));
+    day_1();
+}
+
+
+fn day_1(){
+    let (calories, elf_index) = find_most_cal_elf(&Path::new(r"./res/day_1/elf_food_list.csv"));
+    println!("Elf number {elf_index} has with {calories} the most calories");
+    let v@[elf_1, elf_2, elf_3] = find_3_most_cal_elfs(&Path::new(r"./res/day_1/elf_food_list.csv"));
+    let sum:usize = v.iter().sum();
+    println!("the most calories are {elf_1}, {elf_2}, {elf_3} with a toatl of {sum}");
 }
